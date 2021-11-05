2144ca373693e9a6ece6a13e6d5438ce04a0fe86
amartinhua2 <93262314+amartinhua2@users.noreply.github.com>
2021-11-05 14:17:53 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+let tenperatura = 0
+music.setVolume(255)
+basic.forever(function () {
+    tenperatura = input.temperature()
+    basic.showNumber(input.temperature())
+    if (tenperatura < 17) {
+        basic.showIcon(IconNames.Chessboard)
+    }
+    if (tenperatura > 17) {
+        basic.showIcon(IconNames.Square)
+    }
+})
+basic.forever(function () {
+    if (tenperatura < 17) {
+        music.playTone(392, music.beat(BeatFraction.Quarter))
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+    }
+    if (tenperatura > 17) {
+        music.playTone(370, music.beat(BeatFraction.Quarter))
+        music.playTone(311, music.beat(BeatFraction.Quarter))
+    }
+})
